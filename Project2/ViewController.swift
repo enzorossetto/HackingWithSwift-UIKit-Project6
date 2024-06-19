bfa929c5db783b423d3259946adee8a60cd931fd
Enzo Rossetto <enzo.rossetto@ifood.com.br>
2024-06-19 18:33:04 -0300

    Day 20
    Making the game work

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -36,8 +36,7 @@ class ViewController: UIViewController {
         askQuestion()
     }
 
-
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
         
@@ -46,6 +45,24 @@ class ViewController: UIViewController {
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         
         title = countries[correctAnswer].uppercased()
+    }
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
     }
 }
 
